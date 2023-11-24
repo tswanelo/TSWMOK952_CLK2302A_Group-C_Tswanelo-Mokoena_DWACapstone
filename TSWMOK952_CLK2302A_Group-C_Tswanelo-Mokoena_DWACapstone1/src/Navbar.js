16847980c5b334516c6580bd975efffb96577587
tswanelo28 <t.mokoena2@lab.clicklearning.org>
2023-11-25 00:28:24 +0200

    DWA2-finalCapstone

---
+++ TSWMOK952_CLK2302A_Group-C_Tswanelo-Mokoena_DWACapstone1/src/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="/podcast.jpg" className="nav--icon"/>
+      <h3 className="nav--logo_text">PodCast</h3>
+    </nav>
+  );
+}
